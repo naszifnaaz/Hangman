7ef14a77d2fe0a6012cfdea6eb5234c3c86bc219
naszifnaaz <naszifnaaz@gmail.com>
2020-11-01 00:44:32 +0530

    added buttons

--- hangman.py
+++ hangman.py
@@ -7,6 +7,32 @@ width, height = 800, 500
 win = pygame.display.set_mode((width,height))
 pygame.display.set_caption(("Hangman"))
 
+#Button variables
+radius = 20
+gap = 15
+letters = []
+startx = round((width - (radius * 2 + gap) * 13) / 2)
+starty = 400
+
+for i in range(26):
+    x = startx + gap * 2 + ((radius * 2 + gap) * (i % 13))
+    y = starty + ((i // 13) * (gap + radius * 2))
+    letters.append([x,y])
+
+
+#Draw function
+def draw():
+    win.fill(white)
+
+    #draw buttons
+    for letter in letters:
+        x, y = letter
+        pygame.draw.circle(win, black, (x,y), radius, 3)
+
+    win.blit(images[hangman_state], (150, 100))
+    pygame.display.update()
+
+
 #Loading assets
 images = []
 for i in range(7):
@@ -17,7 +43,7 @@ print(images)
 #Game variables
 hangman_state = 0
 
-#Color codes
+#Colors
 white = (255,255,255)
 black = (0,0,0)
 
@@ -28,9 +54,7 @@ run = True
 
 while run:
     clock.tick(FPS)
-    win.fill(white)
-    win.blit(images[hangman_state], (150, 100))
-    pygame.display.update()
+    draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
