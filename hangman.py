ec04fc362dc8b0abe72bf48cfb53e7f0aa5c41d5
naszifnaaz <naszifnaaz@gmail.com>
2020-11-01 12:30:59 +0530

    added letters

--- hangman.py
+++ hangman.py
@@ -10,14 +10,15 @@ pygame.display.set_caption(("Hangman"))
 #Button variables
 radius = 20
 gap = 15
-letters = []
+letters = []    #triplet with x,y co ordinates and letter ascii value
 startx = round((width - (radius * 2 + gap) * 13) / 2)
 starty = 400
+A =65
 
 for i in range(26):
     x = startx + gap * 2 + ((radius * 2 + gap) * (i % 13))
     y = starty + ((i // 13) * (gap + radius * 2))
-    letters.append([x,y])
+    letters.append([x, y, chr(A + i)])
 
 
 #Draw function
@@ -26,11 +27,17 @@ def draw():
 
     #draw buttons
     for letter in letters:
-        x, y = letter
+        x, y, char = letter
         pygame.draw.circle(win, black, (x,y), radius, 3)
+        text = LETTER_FONT.render(char,1,black)
+        win.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
 
     win.blit(images[hangman_state], (150, 100))
     pygame.display.update()
+
+
+#Fonts
+LETTER_FONT = pygame.font.SysFont('comicsans',40)
 
 
 #Loading assets
@@ -41,7 +48,7 @@ for i in range(7):
 print(images)
 
 #Game variables
-hangman_state = 0
+hangman_state = 6
 
 #Colors
 white = (255,255,255)
